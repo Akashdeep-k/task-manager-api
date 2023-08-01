12d808d0ceaddf88370ed3f89344c6c1ffc6c60f
Akash <iamakashkataria18@gmail.com>
2023-08-01 23:38:58 +0530

    added resource reading endpoints-user and task

--- src/app.js
+++ src/app.js
@@ -6,7 +6,49 @@ const taskModel = require("./db/models/task.js");
 const app = express()
 const port = process.env.PORT || 3000;
 
-app.use(express.json()) 
+app.use(express.json());
+
+app.get("/users", (req, res) => {
+    userModel.find({}).then((users) => {
+        res.send(users);
+    }).catch((e) => {
+        res.status(500).send(e);
+    });
+})
+
+app.get("/users/:userid", (req, res) => {
+    const { userid } = req.params;
+    userModel.findById(userid).then((user) => {
+        if (!user) {
+            return res.status(404).send()
+        }
+        res.send(user);
+
+    }).catch((e) => {
+        res.status(500).send(e);
+    })
+});
+
+app.get("/tasks", (req, res) => {
+    taskModel.find({}).then((tasks) => {
+        res.send(tasks);
+    }).catch((e) => {
+        res.status(500).send(e);
+    });
+})
+
+app.get("/tasks/:taskid", (req, res) => {
+    const { taskid } = req.params;
+    taskModel.findById(taskid).then((task) => {
+        if (!task) {
+            return res.status(404).send()
+        }
+        res.send(task);
+
+    }).catch((e) => {
+        res.status(500).send(e);
+    })
+});
 
 app.post("/users", (req, res) => {
     const user = new userModel(req.body);
