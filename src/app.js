40d8e33bde6560e907ea548068de9e6b68c44e4f
Akash <iamakashkataria18@gmail.com>
2023-08-01 21:36:55 +0530

    added resource creation endpoints-user and task

--- src/app.js
+++ src/app.js
@@ -1,12 +1,33 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+require("./db/mongoose.js");
+const userModel = require("./db/models/user.js");
+const taskModel = require("./db/models/task.js");
 
+const app = express()
 const port = process.env.PORT || 3000;
 
-app.get("/users", (req, res)=>{
-    res.send("Testing !")
+app.use(express.json()) 
+
+app.post("/users", (req, res) => {
+    const user = new userModel(req.body);
+
+    user.save().then(() => {
+        res.status(201).send(user);
+    }).catch((e) => {
+        res.status(400).send(e);
+    })
 })
 
-app.listen(3000, ()=>{
+app.post("/tasks", (req, res) => {
+    const task = new taskModel(req.body);
+
+    task.save().then(() => {
+        res.status(201).send(task);
+    }).catch((e) => {
+        res.status(400).send(e);
+    })
+})
+
+app.listen(3000, () => {
     console.log("Server is running on port " + port)
 })
