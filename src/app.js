e50ead21134bc1704f290fbad6f94408be7f5131
Akash <iamakashkataria18@gmail.com>
2023-08-02 15:09:32 +0530

    integrating async-await in route handlers

--- src/app.js
+++ src/app.js
@@ -8,67 +8,73 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get("/users", (req, res) => {
-    userModel.find({}).then((users) => {
+app.get("/users", async (req, res) => {
+    try {
+        const users = await userModel.find({});
         res.send(users);
-    }).catch((e) => {
+    } catch (e) {
         res.status(500).send(e);
-    });
-})
+    }
+});
 
-app.get("/users/:userid", (req, res) => {
-    const { userid } = req.params;
-    userModel.findById(userid).then((user) => {
+app.get("/users/:userid", async (req, res) => {
+    try {
+        const { userid } = req.params;
+        const user = await userModel.findById(userid);
+
         if (!user) {
-            return res.status(404).send()
+            return res.status(404).send();
         }
+
         res.send(user);
-
-    }).catch((e) => {
+    } catch (e) {
         res.status(500).send(e);
-    })
+    }
 });
 
-app.get("/tasks", (req, res) => {
-    taskModel.find({}).then((tasks) => {
+app.get("/tasks", async (req, res) => {
+    try {
+        const tasks = await taskModel.find({});
         res.send(tasks);
-    }).catch((e) => {
+    } catch (e) {
         res.status(500).send(e);
-    });
-})
-
-app.get("/tasks/:taskid", (req, res) => {
-    const { taskid } = req.params;
-    taskModel.findById(taskid).then((task) => {
-        if (!task) {
-            return res.status(404).send()
-        }
-        res.send(task);
-
-    }).catch((e) => {
-        res.status(500).send(e);
-    })
+    }
 });
 
-app.post("/users", (req, res) => {
-    const user = new userModel(req.body);
+app.get("/tasks/:taskid", async (req, res) => {
+    try {
+        const { taskid } = req.params;
+        const task = await taskModel.findById(taskid);
 
-    user.save().then(() => {
-        res.status(201).send(user);
-    }).catch((e) => {
+        if (!task) {
+            return res.status(404).send();
+        }
+
+        res.send(task);
+    } catch (e) {
+        res.status(500).send();
+    }
+});
+
+app.post("/users", async (req, res) => {
+    try{
+        const user = new userModel(req.body);
+        await user.save();
+        res.send(user);
+    } catch(e){
         res.status(400).send(e);
-    })
-})
+    }
+});
 
-app.post("/tasks", (req, res) => {
-    const task = new taskModel(req.body);
-
-    task.save().then(() => {
-        res.status(201).send(task);
-    }).catch((e) => {
+app.post("/tasks", async(req, res) => {
+    try{
+        const task = taskModel(req.body);
+        await task.save();
+        res.send(task);
+    } catch(e){
         res.status(400).send(e);
-    })
-})
+    }
+});
 
 app.listen(3000, () => {
     console.log("Server is running on port " + port)
