22c0ec0b355eb4d9ac2c0e4722ad72ab3ef4c2d6
Akash <iamakashkataria18@gmail.com>
2023-08-17 08:51:46 +0530

    hiding private data

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -56,7 +56,16 @@ userSchema.methods.getAuthToken = async function () {
     await user.save();
 
     return token;
+}
 
+userSchema.methods.toJSON = function(){
+    const user = this;
+    const userObject = user.toObject();
+
+    delete userObject.password;
+    delete userObject.tokens;
+
+    return userObject;
 }
 
 userSchema.statics.findByCredentials = async (email, password) => {
