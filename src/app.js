b23d0d0ce793478ddc19db2c7f49d73c51121df6
Akash <iamakashkataria18@gmail.com>
2023-08-21 13:20:09 +0530

    refactoring code before using supertest

--- src/app.js
+++ src/app.js
@@ -4,12 +4,9 @@ const userRouter = require("./routers/user.js");
 const taskRouter = require("./routers/task.js");
 
 const app = express()
-const port = process.env.PORT;
 
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
 
-app.listen(3000, () => {
-    console.log("Server is running on port " + port)
-})
+module.exports = { app };
